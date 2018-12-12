9cd5d429b117a21455a52bfb61114b0c1a88e0e3
ymendes <mendesyuri@poli.ufrj.br>
2018-12-12 13:13:13 -0200

    ultimas mudanças

--- flaskr.py
+++ flaskr.py
@@ -1,13 +1,25 @@
 # coding: utf-8
 
-
+import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 from contextlib import closing
 
+DATABASE = '/tmp/flaskr.db'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+def conectar_bd():
+    return sqlite3.connect(app.config['DATABASE'])
+
+def criar_bd():
+    with closing(conectar_bd()) as bd:
+        with app.open_resource('esquema.sql') as sql:
+            bd.cursor().executescript(sql.read())
+        bd.commit()
+
+
+        
 @app.route("/")
 def inicial():
   return render_template("Initial.html")
